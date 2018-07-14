8a458ec37d26dae8c976b82b038475d61c8c67f0
nathalie <nathalie.scruz@gmail.com>
2018-07-14 19:23:41 +0000

    automatically backed up by learn

--- lib/user.rb
+++ lib/user.rb
@@ -3,7 +3,8 @@ class User
   attr_accessor :first_name
 
   def initialize(first_name, last_name)
-    @first_name, @last_name = first_name, last_name
+    @first_name= first_name
+    @last_name= last_name
   end
 
 end
