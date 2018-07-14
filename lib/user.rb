4406d5b8067ca6ab19d89f122356fb4fbe44991f
nathalie <nathalie.scruz@gmail.com>
2018-07-14 19:26:38 +0000

    automatically backed up by learn

--- lib/user.rb
+++ lib/user.rb
@@ -3,11 +3,6 @@ require 'pry'
 class User
 
   attr_accessor :first_name, :last_name
-binding.pry
-  def initialize(first_name, last_name)
-    
-    @first_name= first_name
-    @last_name= last_name
-  end
+
 
 end
